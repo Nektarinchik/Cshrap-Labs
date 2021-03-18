cc298beb751afb5d71fd8ff7f4f72c3204895591
unknown <ermolen2003@gmail.com>
2021-03-19 01:34:38 +0300

    lab3

---
+++ LR3/Class1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    public class Vehicle
+    {
+        public enum Сolors
+        {
+            blue,
+            yellow,
+            grey,
+            white,
+            black,
+            brown,
+            green,
+            red,
+            purple,
+            pink
+        }
+        public enum EngineTypes
+        {
+            internalCombustionEngine,
+            electric
+        }
+        public enum FuelTypes
+        {
+            gasoline,
+            diesel,
+            gas,
+            battery
+        }
+        public Сolors Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+        public EngineTypes EngineType
+        {
+            get
+            {
+                return engineType;
+            }
+            set
+            {
+                engineType = value;
+            }
+        }
+        public FuelTypes FuelType
+        {
+            get
+            {
+                return fuelType;
+            }
+            set
+            {
+                fuelType = value;
+            }
+        }
+        public double EngineCapacity
+        {
+            get
+            {
+                return engineСapacity;
+            }
+            set
+            {
+                engineСapacity = value;
+            }
+        }
+        public int Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                weight = value;
+            }
+        }
+        public double FuelСonsumption
+        {
+            get
+            {
+                return fuelConsumption;
+            }
+            set
+            {
+                fuelConsumption = value;
+            }
+        }
+        public int SeatsAmount
+        {
+            get
+            {
+                return seatsAmount;
+            }
+            set
+            {
+                seatsAmount = value;
+            }
+        }
+        public int YearOfRelease
+        {
+            get
+            {
+                return yearOfRelease;
+            }
+            set
+            {
+                yearOfRelease = value;
+            }
+        }
+
+        private Сolors color;
+        private EngineTypes engineType;
+        private FuelTypes fuelType;
+        private double engineСapacity;
+        private double fuelConsumption;
+        private int weight;
+        private int seatsAmount;
+        private int yearOfRelease;
+    }
+
+}
---
+++ LR3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle auto = new Vehicle();
+            auto.Color = Vehicle.Сolors.black;
+            auto.EngineType = Vehicle.EngineTypes.internalCombustionEngine;
+            auto.FuelType = Vehicle.FuelTypes.gasoline;
+            auto.EngineCapacity = 2.2;
+            auto.FuelСonsumption = 9.7;
+            auto.SeatsAmount = 4;
+            auto.Weight = 1960;
+            auto.YearOfRelease = 2014;
+            Console.WriteLine($"Color: {auto.Color}");
+            Console.WriteLine($"Type of engine: {auto.EngineType}");
+            Console.WriteLine($"Type of fuel: {auto.FuelType}");
+            Console.WriteLine($"Engine capacity: {auto.EngineCapacity}");
+            Console.WriteLine($"Fuel consumption: {auto.FuelСonsumption}");
+            Console.WriteLine($"Number of seats: {auto.SeatsAmount}");
+            Console.WriteLine($"Weight: {auto.Weight}");
+            Console.WriteLine($"Year of release: {auto.YearOfRelease}");
+
+
+        }
+    }
+}
